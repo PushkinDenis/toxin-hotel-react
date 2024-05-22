79c15f09d17429d5fc8d6a4f2a07a1cf41340389
Denis <denisp9831@gmail.com>
2024-05-22 16:36:27 +1000

    colors and type component fix

--- src/pages/ui-kit/colors-type/ui/colors-type.tsx
+++ src/pages/ui-kit/colors-type/ui/colors-type.tsx
@@ -4,70 +4,29 @@ import { ColorBox } from "@entities";
 import { LayoutUiKit } from "@pages";
 import styles from "./colors-type.module.scss";
 export const ColorsAndType: FC = () => {
-  const colorArr = ["var(--color-dark-shade-100)", "var(--color-dark-shade-75)", "var(--color-dark-shade-50)", "var(--color-dark-shade-25)", "var(--color-dark-shade-5)", "var(--color-purple)", "var(--color-green)"];
+  const colorBlockArr = [
+    { color: "var(--color-dark-shade-100)", h2: "Dark Shade 100%", p: "#1F2041" },
+    { color: "var(--color-dark-shade-75)", h2: "Dark Shade 75%", p: "#1F2041" },
+    { color: "var(--color-dark-shade-50)", h2: "Dark Shade 50%", p: "#1F2041" },
+    { color: "var(--color-dark-shade-25)", h2: "Dark Shade 25%", p: "#1F2041" },
+    { color: "var(--color-dark-shade-5)", h2: "Dark Shade 5%", p: "#1F2041" },
+    { color: "var(--color-purple)", h2: "Purple", p: "#BC9CFF" },
+    { color: "var(--color-green)", h2: "Green", p: "#6FCF97" },
+  ];
   return (
     <LayoutUiKit>
       <div className={clsx(styles.color)}>
-        {colorArr.map((value) => {
+        {colorBlockArr.map((value) => {
           return (
             <div className={clsx(styles["color-block"])}>
-              <ColorBox backgroundColor={value} />
+              <ColorBox backgroundColor={value.color} />
               <div className={clsx(styles["color-label"])}>
-                <h2>Dark Shade 100%</h2>
-                <p>#1F2041</p>
+                <h2>{value.h2}</h2>
+                <p>{value.p}</p>
               </div>
             </div>
           );
         })}
-        {/* <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-dark-shade-100)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Dark Shade 100%</h2>
-            <p>#1F2041</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-dark-shade-75)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Dark Shade 75%</h2>
-            <p>#1F2041</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-dark-shade-50)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Dark Shade 50%</h2>
-            <p>#1F2041</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-dark-shade-25)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Dark Shade 25%</h2>
-            <p>#1F2041</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-dark-shade-5)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Dark Shade 5%</h2>
-            <p>#1F2041</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-purple)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Purple </h2>
-            <p>#BC9CFF</p>
-          </div>
-        </div>
-        <div className={clsx(styles["color-block"])}>
-          <ColorBox backgroundColor="var(--color-green)" />
-          <div className={clsx(styles["color-label"])}>
-            <h2>Green </h2>
-            <p>#6FCF97</p>
-          </div>
-        </div> */}
       </div>
     </LayoutUiKit>
   );
