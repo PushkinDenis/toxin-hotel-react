c401bb481ab64d8c1fea7442814572a25ba75ccc
Denis <denisp9831@gmail.com>
2024-05-31 13:31:16 +1000

    dropdown style fix

--- src/pages/ui-kit/form-elements/ui/form-elements.tsx
+++ src/pages/ui-kit/form-elements/ui/form-elements.tsx
@@ -8,7 +8,7 @@ export const FormElements: FC = () => {
       <TextField placeholder={"Email"} />
       <TextField placeholder={"Email"} />
       <Dropdown items={["ВЗРОСЛЫЕ", "ДЕТИ", "МЛАДЕНЦЫ"]} />
-      <Dropdown items={["Кровать", "Диван", "Спальня"]} />
+      <Dropdown items={["СПАЛЬНИ", "КРОВАТИ", "ВАННЫЕ КОМНАТЫ"]} />
     </LayoutUiKit>
   );
 };
