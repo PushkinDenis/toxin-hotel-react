ee77fd3a169dca74240cd955f64ec77d2165a36e
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-06-01 17:21:54 +1000

    test

--- src/pages/ui-kit/form-elements/ui/form-elements.tsx
+++ src/pages/ui-kit/form-elements/ui/form-elements.tsx
@@ -3,6 +3,7 @@ import { LayoutUiKit } from "@pages";
 import { Dropdown, TextField } from "@shared";
 
 export const FormElements: FC = () => {
+  console.log("frf");
   return (
     <LayoutUiKit>
       <TextField placeholder={"Email"} />
