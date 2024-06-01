1cbd0d6b7f2262752335c1a15ed7e703ebac8a2f
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-06-02 00:07:27 +1000

    Updates

--- src/pages/ui-kit/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./colors-type/ui/colors-type.tsx";
-export * from "./form-elements/ui/form-elements.tsx";
-export * from "./layout/ui/layout-ui-kit.tsx";
--- src/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ui/text-field/text-field";
-export * from "./ui/dropdown/dropdown";
-export * from "./ui/counter/counter.tsx";
-export * from "./ui/button/button.tsx";
