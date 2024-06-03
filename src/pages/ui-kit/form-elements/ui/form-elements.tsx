a0ece85af65b0aed49cb94bb092efecf3c228105
Denis <denisp9831@gmail.com>
2024-06-03 11:10:21 +1000

    responsive fix

--- src/pages/ui-kit/form-elements/ui/form-elements.tsx
+++ src/pages/ui-kit/form-elements/ui/form-elements.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import { LayoutUiKit } from "@pages";
 import { Dropdown, TextField } from "@shared";
+import styles from "./form-elements.module.scss";
 
 export const FormElements: FC = () => {
   return (
     <LayoutUiKit>
-      <TextField placeholder={"Email"} />
-      <TextField placeholder={"Email"} />
-      <Dropdown items={["ВЗРОСЛЫЕ", "ДЕТИ", "МЛАДЕНЦЫ"]} />
-      <Dropdown items={["СПАЛЬНИ", "КРОВАТИ", "ВАННЫЕ КОМНАТЫ"]} />
+      <div className={styles.wrapper}>
+        <TextField placeholder={"Email"} />
+        <TextField placeholder={"Email"} />
+        <Dropdown items={["ВЗРОСЛЫЕ", "ДЕТИ", "МЛАДЕНЦЫ"]} />
+        <Dropdown items={["СПАЛЬНИ", "КРОВАТИ", "ВАННЫЕ КОМНАТЫ"]} />
+      </div>
     </LayoutUiKit>
   );
 };
