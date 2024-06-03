1d1c29d04b4397bf8c028c856ee593633ebcca7e
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-06-03 20:26:31 +1000

    datepicker fix

--- src/shared/ui/daterange-picker/daterange-picker.tsx
+++ src/shared/ui/daterange-picker/daterange-picker.tsx
@@ -1,15 +1,43 @@
-import { DatePicker } from "@mantine/dates";
-import { useState } from "react";
+import { DatePicker, DatePickerBaseProps, DatesProvider } from "@mantine/dates";
+import { FC, useState } from "react";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
+import "dayjs/locale/ru";
+import styles from "./daterange-picker.module.scss";
 
-export const Demo = () => {
+interface CustomDatePickerProps extends DatePickerBaseProps {
+  classNames?: {
+    wrapper?: string;
+    calendarHeader?: string;
+    calendarHeaderLevel?: string;
+    day?: string;
+    daySelected?: string;
+    tbody?: string;
+  };
+}
+
+export const Demo: FC<CustomDatePickerProps> = (props) => {
   const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
 
   return (
-    <MantineProvider withCssVariables withGlobalClasses withStaticClasses>
-      <DatePicker type="range" value={value} onChange={setValue} />{" "}
+    <MantineProvider>
+      <DatesProvider settings={{ locale: "ru" }}>
+        <DatePicker
+          type="range"
+          value={value}
+          onChange={setValue}
+          classNames={{
+            wrapper: styles.mantineDatePickerWrapper,
+            calendarHeader: styles.mantineDatePickerCalendarHeader,
+            calendarHeaderLevel: styles.mantineDatePickerCalendarHeaderLevel,
+            day: styles.mantineDatePickerDay,
+            daySelected: styles.mantineDatePickerDaySelected,
+            tbody: styles["mantine-DatePicker-month"],
+          }}
+          {...props}
+        />
+      </DatesProvider>
     </MantineProvider>
   );
 };
