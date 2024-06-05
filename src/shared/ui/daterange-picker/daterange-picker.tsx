bf78134afbd8f52fbe6d31d1cf7e46f9fd90538f
Denis <denisp9831@gmail.com>
2024-06-05 11:15:17 +1000

    datepicker style fix

--- src/shared/ui/daterange-picker/daterange-picker.tsx
+++ src/shared/ui/daterange-picker/daterange-picker.tsx
@@ -6,6 +6,7 @@ import "@mantine/dates/styles.css";
 import "dayjs/locale/ru";
 import styles from "./daterange-picker.module.scss";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
+import { Button } from "@shared";
 
 interface CustomDatePickerProps {
   classNames?: {
@@ -24,25 +25,31 @@ export const Demo: FC<CustomDatePickerProps> = (props) => {
 
   return (
     <MantineProvider>
-      <DatesProvider settings={{ locale: "ru" }}>
-        <DatePicker
-          previousIcon={<ArrowBack sx={{ fontSize: "24px", color: "var(--color-purple)" }} />}
-          nextIcon={<ArrowForward sx={{ fontSize: "24px", color: "var(--color-purple)" }} />}
-          type="range"
-          value={value}
-          onChange={setValue}
-          classNames={{
-            wrapper: styles.mantineDatePickerWrapper,
-            calendarHeader: styles.mantineDatePickerCalendarHeader,
-            calendarHeaderLevel: styles.mantineDatePickerCalendarHeaderLevel,
-            day: styles.mantineDatePickerDay,
-            daySelected: styles.mantineDatePickerDaySelected,
-            weekdaysRow: styles.mantineDatePickerweekday,
-            tbody: styles["mantine-DatePicker-month"],
-          }}
-          {...props}
-        />
-      </DatesProvider>
+      <div className={styles.wrapper}>
+        <DatesProvider settings={{ locale: "ru" }}>
+          <DatePicker
+            previousIcon={<ArrowBack sx={{ fontSize: "24px", color: "var(--color-purple)" }} />}
+            nextIcon={<ArrowForward sx={{ fontSize: "24px", color: "var(--color-purple)" }} />}
+            type="range"
+            value={value}
+            onChange={setValue}
+            classNames={{
+              wrapper: styles.mantineDatePickerWrapper,
+              calendarHeader: styles.mantineDatePickerCalendarHeader,
+              calendarHeaderLevel: styles.mantineDatePickerCalendarHeaderLevel,
+              day: styles.mantineDatePickerDay,
+              daySelected: styles.mantineDatePickerDaySelected,
+              weekdaysRow: styles.mantineDatePickerweekday,
+              tbody: styles["mantine-DatePicker-month"],
+            }}
+            {...props}
+          />
+          <div className={styles["buttons-wrapper"]}>
+            <Button variant="text" text="ОЧИСТИТЬ" onClick={() => {}} />
+            <Button variant="text" text="ПРИМЕНИТЬ" onClick={() => {}} />
+          </div>
+        </DatesProvider>
+      </div>
     </MantineProvider>
   );
 };
