368e9374843554bddfd2940688926513ce02f4bb
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-06-05 21:53:20 +1000

    datepicker dropdown fix

--- src/shared/ui/datepicker-dropdown/datepicker-dropdown.tsx
+++ src/shared/ui/datepicker-dropdown/datepicker-dropdown.tsx
@@ -1,26 +1,21 @@
-import { FC } from "react";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-
-import Select from "@mui/material/Select";
-import { RangeDatePicker } from "@shared";
-import { StyledEngineProvider, SvgIcon } from "@mui/material";
-import styles from "../../ui/dropdown/dropdown.module.scss";
-import { ExpandMore } from "@mui/icons-material";
+import { FC, useState } from "react";
+import { RangeDatePicker, TextField } from "@shared";
+import styles from "./datepicker-dropdown.module.scss";
 
 export const DatePickerDropdown: FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = (open: boolean) => {
+    setOpen(open);
+  };
+
   return (
     <>
-      <StyledEngineProvider injectFirst>
-        <FormControl className={styles["form-control"]} sx={{ "& fieldset": { border: "none" } }}>
-          <Select IconComponent={() => <SvgIcon className={styles.icon} component={ExpandMore}></SvgIcon>} className={styles.select} labelId="demo-multiple-checkbox-label" id="demo-multiple-checkbox" input={<OutlinedInput label="Tag" />} multiple value={[]} placeholder={"Всего гостей"}>
-            <MenuItem sx={{ background: "none", border: "none", margin: "0", padding: "0", boxShadow: "none", width: "320px", height: "369px" }}>
-              <RangeDatePicker />
-            </MenuItem>
-          </Select>
-        </FormControl>
-      </StyledEngineProvider>
+      <div className={styles["inputs-wrapper"]}>
+        <TextField onClick={handleOpen} />
+        <div>{open}</div>
+      </div>
+      {open ? <RangeDatePicker /> : false}
     </>
   );
 };
--- src/shared/ui/text-field/text-field.tsx
+++ src/shared/ui/text-field/text-field.tsx
@@ -1,16 +1,30 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./text-field.module.scss";
 import { FormControl, OutlinedInput, StyledEngineProvider } from "@mui/material";
 
 type TextFieldProps = {
   placeholder?: string;
+  onClick?: (open: boolean) => void;
 };
-export const TextField: FC<TextFieldProps> = ({ placeholder }) => {
+export const TextField: FC<TextFieldProps> = ({ placeholder, onClick }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    if (open) {
+      setOpen(false);
+      onClick!(false);
+    } else {
+      setOpen(true);
+      onClick!(true);
+    }
+  };
+
   return (
     <>
       <StyledEngineProvider injectFirst>
         <FormControl className={styles["form-control"]} sx={{ "& fieldset": { border: "none" } }}>
           <OutlinedInput
+            onClick={handleOpen}
             className={styles.input}
             placeholder={placeholder}
             sx={{
