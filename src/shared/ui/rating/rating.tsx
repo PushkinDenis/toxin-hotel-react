68ff42e64f8daf048eebcf43c65eca424cb56a4a
Denis <denisp9831@gmail.com>
2024-06-11 10:36:19 +1000

    rating style fix

--- src/shared/ui/rating/rating.tsx
+++ src/shared/ui/rating/rating.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
-import styles from "./rating.module.scss";
-import { Rating as RatingMui, StyledEngineProvider, FormControlLabel } from "@mui/material";
+import { Rating as RatingMui, StyledEngineProvider } from "@mui/material";
 import { RateStar, RateStarChecked } from "@shared";
+import styles from "./rating.module.scss";
+
 export const Rating: FC = () => {
   return (
     <>
       <StyledEngineProvider injectFirst>
-        <RatingMui icon={<RateStarChecked />} emptyIcon={<RateStar />} precision={0.5} />
+        <RatingMui className={styles.rating} icon={<RateStarChecked />} emptyIcon={<RateStar />} precision={0.5} />
       </StyledEngineProvider>
     </>
   );
