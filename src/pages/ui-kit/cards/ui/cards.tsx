b468b323323e45b4235d26dff2f4cddc8d1dc17a
Denis <denisp9831@gmail.com>
2024-06-19 13:28:57 +1000

    cards layout fix

--- src/pages/ui-kit/cards/ui/cards.tsx
+++ src/pages/ui-kit/cards/ui/cards.tsx
@@ -3,17 +3,25 @@ import { CardsLayout } from "@pages";
 import { FindRoomCard, SignUpCard, LogInCard, BillCard, RoomCard } from "@widgets";
 import { RangeDatePicker } from "@shared";
 import styles from "./cards.module.scss";
+import clsx from "clsx";
+
 export const Cards: FC = () => {
   return (
     <CardsLayout>
-      <RoomCard type="photos" roomNumber={"№ 888"} roomType={"люкс"} price={"9 990₽ "} feedbackNumber={"145"} image={"./pictures/room_1.png"} />
-      <RoomCard roomNumber={"№ 840"} roomType={""} price={"9 990₽ "} feedbackNumber={"65"} image={"./pictures/room_2.png"} />
-      <FindRoomCard />
-      <SignUpCard />
-      <LogInCard />
-      <BillCard />
-      <div className={styles.date_wrapper}>
-        <RangeDatePicker isStatic />
+      <div className={clsx(styles.column, styles.column_first)}>
+        <FindRoomCard />
+        <SignUpCard />
+      </div>
+      <div className={clsx(styles.column, styles.column_second)}>
+        <BillCard />
+        <LogInCard />
+      </div>
+      <div className={clsx(styles.column, styles.column_third)}>
+        <div className={styles.date_wrapper}>
+          <RangeDatePicker isStatic />
+        </div>
+        <RoomCard type="photos" roomNumber={"№ 888"} roomType={"люкс"} price={"9 990₽ "} feedbackNumber={"145"} image={"./pictures/room_1.png"} />
+        <RoomCard roomNumber={"№ 840"} roomType={""} price={"9 990₽ "} feedbackNumber={"65"} image={"./pictures/room_2.png"} />
       </div>
     </CardsLayout>
   );
