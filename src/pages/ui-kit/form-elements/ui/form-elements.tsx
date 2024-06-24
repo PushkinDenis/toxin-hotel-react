cc214138f235a6be1f973418d255115545adb9f3
Denis <denisp9831@gmail.com>
2024-06-24 16:35:27 +1000

    quick fix

--- src/pages/ui-kit/form-elements/ui/form-elements.tsx
+++ src/pages/ui-kit/form-elements/ui/form-elements.tsx
@@ -22,7 +22,7 @@ export const FormElements: FC = () => {
             <ElementLayout label={"masked text field"}>
               <TextField placeholder={"ДД.ММ.ГГГГ"} />
             </ElementLayout>
-            <DatePickerDropdown type={"date"} label_first="date dropdown" label_second="date dropdown" defaultSecondValue={"19.08.2019"} />
+            <DatePickerDropdown type={"date"} label_first="date dropdown" label_second="date dropdown" />
             <ElementLayout label={"filter date dropdown"}>
               <DatePickerDropdown type={"date-wide"} />
             </ElementLayout>
